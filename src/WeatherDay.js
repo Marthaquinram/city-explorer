1f82f6af12f2280d83037ab92661cbc562db36ed
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-05-24 00:05:30 -0700

    started the Refactor: Componentize the front-end codebase

---
+++ src/WeatherDay.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import "./App.css";
+
+// class weatherDay extends React.Component {
+
+//     render() {
+//     return (
+//       <div className="App">
+    
+//       </div>
+//     );
+//   }
+// }
+
+// export default weatherDay;
