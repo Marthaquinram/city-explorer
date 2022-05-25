bda371612f6cadb55a64e0a782fbb0c5bbf9ffe0
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-05-24 18:31:46 -0700

    completed lab 9

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,23 @@
-// import React from "react";
+import React from "react";
+import { Card } from "react-bootstrap";
 
 
-// class Movie extends React.Component {
-//   render() {
-//     return (
-//   <>
-  
-//   </>
-//     );
-//   }
-// }
+class Movie extends React.Component {
+  render() {
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title >
+            {this.props.movie.title}
+          </Card.Title>
+          <Card.Img key={this.props.idx} src={this.props.movie.poster}/>
+          <Card.Text>
+            {this.props.movie.description}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 
-// export default Movie;
+export default Movie;
--- src/Movies.js
+++ src/Movies.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import Movie from './Movie';
+
 class Movies extends React.Component {
   render() {
     return (
-  <>
+  <div>
+   
   {
-  this.props.movieSug
-}
-  </>
+  this.props.movieSug && this.props.movieSug.map((movie, idx) => <Movie movie={movie} idx={idx}/>)
+  }
+  
+  </div>
+
     );
   }
 }
